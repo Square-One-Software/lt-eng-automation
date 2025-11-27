f4f3591395f59d0674bc16584993b89b489f7740
Louis Tsang <kawingt2@gmail.com>
2025-11-27 12:33:03 +0800

    expand main args and add tuition note functions to generate tuition debit note

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from utils import parse_vocab_file 
+from utils import parse_vocab_file, parse_tuition_file, get_month_and_month_name 
 from pdf_utils import generate_vocabulary_pdf
+from tuition_debit_note import generate_tuition_debit_note
 import argparse, asyncio
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="Vocabulary Generator", description="Generate a vocabulary list PDF.")
+    VC, TU = "vc", "tu"
+    parser = argparse.ArgumentParser(prog="LT ENG PDF Generator", description="Generate PDF for vocabulary list or tuition debit note.")
+    parser.add_argument('-t', '--type', type=str, choices=[VC, TU], required=True, default="vc", help="Type of PDF to generate: vc = vocab list, tu = tuition debit note")
     parser.add_argument('-o', '--output', type=str, default="vocabulary_list.pdf",
                         help="Output PDF filename (default: vocabulary_list.pdf)")
     parser.add_argument('-f', '--file', type=str, help="Input vocabulary csv file name (vocab.csv)", required=True)
     # Parse arguments
     args = parser.parse_args()
-    output_filename = args.output
     csv_filename = args.file
-
-    vocab_data = parse_vocab_file(csv_filename)
-    
-    # Generate PDF
-    asyncio.run(generate_vocabulary_pdf(output_filename, vocab_data))
-
+    if args.type == VC: 
+        output_filename = args.output
+        vocab_data = parse_vocab_file(csv_filename)
+        # Generate PDF
+        asyncio.run(generate_vocabulary_pdf(output_filename, vocab_data))
+    else:
+        lesson_data, course_desc, student_name = parse_tuition_file(csv_filename)
+        month, month_name = get_month_and_month_name()
+        generate_tuition_debit_note(
+            filename=f"{student_name}_{month_name}_2025.pdf",
+            student_name=student_name,
+            month=f"{month}月",
+            lessons=lesson_data,
+            lesson_desc=course_desc,
+            notes=""
+        )
 if __name__ == "__main__":
     main()
