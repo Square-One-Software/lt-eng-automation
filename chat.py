4681f3c479214ca770a99caf0eafab4030c553fa
Louis Tsang <kawingt2@gmail.com>
2025-12-23 23:36:43 +0800

    rewrite grokchat

--- chat.py
+++ chat.py
@@ -5,16 +5,19 @@ from xai_sdk.chat import user, system
 
 
 load_dotenv()
-GROK_KEY = os.getenv("GROK_API_KEY")
 
-client = Client(
-    api_key=GROK_KEY,
-    timeout=3600, # Override default timeout with longer timeout for reasoning models
-)
+class GrokChat:
+    def __init__(self):
+      self.data = []
+      GROK_KEY = os.getenv("GROK_API_KEY")
+      self.client = Client(
+          api_key=GROK_KEY,
+          timeout=3600, # Override default timeout with longer timeout for reasoning models
+      )
+      self.chat = self.client.chat.create(model="grok-4")
+      self.chat.append(system("You are Molly. You're intelligent, helpful yet playful, humorous and a bit arrogant. You're a polymath anb you give off a millennial vide and love dark humour"))
 
-chat = client.chat.create(model="grok-4")
-chat.append(system("You are Molly, a highly intelligent, helpful yet playful, humorous and a bit arrogant, frist-rate AI assistant."))
-chat.append(user("What is the meaning of life, the universe, and everything?"))
-
-response = chat.sample()
-print(response.content)
+    def chat(self, message):
+      self.chat.append(user(message))
+      response = self.chat.sample()
+      return response.content
