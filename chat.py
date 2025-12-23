8be3b458ff446c995c8ffd961b00a376fceef453
Louis Tsang <kawingt2@gmail.com>
2025-12-24 00:08:33 +0800

    make chat work

--- chat.py
+++ chat.py
@@ -8,16 +8,15 @@ load_dotenv()
 
 class GrokChat:
     def __init__(self):
-      self.data = []
       GROK_KEY = os.getenv("GROK_API_KEY")
       self.client = Client(
           api_key=GROK_KEY,
           timeout=3600, # Override default timeout with longer timeout for reasoning models
       )
-      self.chat = self.client.chat.create(model="grok-4")
-      self.chat.append(system("You are Molly. You're intelligent, helpful yet playful, humorous and a bit arrogant. You're a polymath anb you give off a millennial vide and love dark humour"))
 
     def chat(self, message):
-      self.chat.append(user(message))
-      response = self.chat.sample()
+      chat = self.client.chat.create(model="grok-4")
+      chat.append(system("You are Molly. You're intelligent, helpful yet playful, humorous and a bit arrogant. You're a polymath anb you give off a millennial vide and love dark humour"))
+      chat.append(user(message))
+      response = chat.sample()
       return response.content
