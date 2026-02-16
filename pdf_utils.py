c7bd8dcf44d168fd793258adba579e5294ad097c
Louis Tsang <kawingt2@gmail.com>
2026-02-16 23:55:36 +0800

    move output to designated dir

--- pdf_utils.py
+++ pdf_utils.py
@@ -266,6 +266,7 @@ def generate_tuition_debit_note(
     # Build PDF
     doc.build(elements)
     print(f"Tuition debit note generated: {filename}")
+    os.rename(f"{filename}", f"tuition_notes/{filename}")
 
 
 # testing
